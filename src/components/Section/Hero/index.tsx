dca136ce626586545f4527e00b9c06ab4e324992
Lucas de Paula <lucas.depaula@smartnx.io>
2023-06-16 16:53:49 -0300

    Prettier buged :/. Better lokin hero section

--- src/components/Section/Hero/index.tsx
+++ src/components/Section/Hero/index.tsx
@@ -1,12 +1,15 @@
 import { motion } from "framer-motion";
 import Lottie from "react-lottie";
 import teamWorkAnimationLottie from "../../../assets/lottie/business-team.json";
+import { useEffect, useRef } from "react";
+import { stackIcons } from "../../icons/stackIcons";
 
 const Text = {
   pt: {
     title: {
       first: "Não só um",
-      last: "Desenvolvedor Web",
+      last: "DESENVOLVEDOR FRONT-END",
+      animationWords: ['DESENVOLVEDOR REACT', 'DESENVOLVEDOR FRONT-END']
     },
     description: `Desenvolver é só uma parte do processo, devemos entender claramente as
     dores do usuário final e manter um time unido com um propósito único
@@ -16,7 +19,8 @@ const Text = {
   en: {
     title: {
       first: "Not just a",
-      last: "Web Developer",
+      last: "FRONT-END DEVELOPER",
+      animationWords: ['REACT DEVELOPER', 'FRONT-END DEVELOPER',]
     },
     description: `Developing is just a part of the process, we must clearly understand
     the pain of the end user and keep a team together with a single
@@ -25,13 +29,60 @@ const Text = {
   },
 } as const;
 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 const HeroSection = ({ lang }: { lang: keyof typeof Text }) => {
+  const intervalRef = useRef<NodeJS.Timer | null>(null)
+  const titleRef = useRef<HTMLHeadingElement>(null)
+  const intRef = useRef<number>(0)
+
+  useEffect(() => {
+    const run = () => {
+      let iteration = 0
+      const animationWords = Text[lang].title.animationWords
+      const wordIndex = intRef.current;
+      if (!titleRef.current) return;
+      if (intervalRef.current) clearInterval(intervalRef.current)
+      intervalRef.current = setInterval(() => {
+        if (!titleRef.current) return
+        titleRef.current.innerText = animationWords[wordIndex].split("").map((letter: any, index: any) => {
+          if (index < iteration) {
+            return animationWords[wordIndex][index];
+          }
+          return letters[Math.floor(Math.random() * 26)]
+        }).join("");
+
+        if (iteration >= animationWords[wordIndex].length) {
+          if (intervalRef.current) clearInterval(intervalRef.current)
+        }
+
+        iteration += 1 / 2;
+      }, 20)
+
+      if (intRef.current === 0) {
+        intRef.current = 1
+      } else {
+        intRef.current = 0
+      }
+    }
+
+    if (!titleRef.current) return
+    titleRef.current.addEventListener('mouseover', run);
+
+    const timeToTime = setInterval(run, 6000)
+
+    return () => {
+      if (timeToTime) clearInterval(timeToTime)
+    }
+  }, [lang])
+
+
   return (
     <section
       id="hero"
       className="min-h-screen w-full flex items-center justify-center sm:px-10"
     >
-      <div className="px-8 z-10 md:mr-80">
+      <div className="px-8 z-10 md:mr-80 w-[910px]">
         <motion.h1
           initial={{ y: 25, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -39,41 +90,55 @@ const HeroSection = ({ lang }: { lang: keyof typeof Text }) => {
             delay: 0.5,
             duration: 0.75,
           }}
-          className="text-3xl sm:text-6xl leading-[110%] tracking-tighter"
+          className="text-3xl sm:text-4xl leading-[110%] tracking-tighter"
         >
-          {Text[lang].title.first}{" "}
-          <span className="font-black">{Text[lang].title.last}</span>
+          {Text[lang].title.first}
+        </motion.h1>
+
+        <motion.h1
+          initial={{ y: 25, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            delay: 0.5,
+            duration: 0.75,
+          }}
+          className="font-black block sm:text-5xl leading-[110%] tracking-tighter" data-value={Text[lang].title.last} ref={titleRef}
+        >
+          {Text[lang].title.last}
         </motion.h1>
 
         <motion.p
           initial={{ y: 25, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{
-            delay: 0.75,
+            delay: 1,
             duration: 0.75,
           }}
           className="max-w-lg mt-8 text-md sm:text-xl"
         >
           {Text[lang].description}
         </motion.p>
-        {/* <motion.button
-          initial={{ y: 25, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            delay: 0.75,
-            duration: 0.75,
-          }}
-          className="cursor-pointer p-2 bg-tertiary rounded-md mt-8 sm:text-lg"
-        >
-          <a href="#contact">{Text[lang].button}</a>
-        </motion.button> */}
+
+        <ul className="flex translate-y-20 gap-16">{stackIcons.map((icon, index) => (
+          <motion.li
+
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.7 + (0.1 * index),
+              duration: 0.75,
+            }}
+
+            title={icon.title} key={icon.title} className="rounded-full p-1 flex items-center justify-center">{icon.icon}</motion.li>
+        ))}</ul>
+
       </div>
       <div className="absolute bottom-0 right-0 opacity-80 flex justify-end">
         <motion.div
           initial={{ y: 25, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{
-            delay: 1,
+            delay: 1.25,
             duration: 0.75,
           }}
           className="md:h-1/2 md:w-1/2 sm:h-96 sm:w-96"
