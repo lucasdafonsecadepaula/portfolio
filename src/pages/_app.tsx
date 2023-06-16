24646ac17b5fc4608d573b19da217b292bac7248
Lucas de Paula <lucas.depaula@smartnx.io>
2023-06-16 17:01:57 -0300

    Adding analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
