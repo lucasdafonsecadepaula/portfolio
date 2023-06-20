58d9e766a4e1e0584efd0ef84f09c779806d8356
Lucas de Paula <lucas.depaula@smartnx.io>
2023-06-20 12:29:55 -0300

    making resposivenes

--- src/components/Section/Hero/index.tsx
+++ src/components/Section/Hero/index.tsx
@@ -82,7 +82,7 @@ const HeroSection = ({ lang }: { lang: keyof typeof Text }) => {
       id="hero"
       className="min-h-screen w-full flex items-center justify-center sm:px-10"
     >
-      <div className="px-8 z-10 md:mr-80 w-[910px]">
+      <div className="sm:px-8 z-10 md:mr-80 sm:w-[910px] pb-24 sm:pb-0 px-2">
         <motion.h1
           initial={{ y: 25, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -90,7 +90,7 @@ const HeroSection = ({ lang }: { lang: keyof typeof Text }) => {
             delay: 0.5,
             duration: 0.75,
           }}
-          className="text-3xl sm:text-4xl leading-[110%] tracking-tighter"
+          className="sm:text-4xl leading-[110%] tracking-tighter"
         >
           {Text[lang].title.first}
         </motion.h1>
@@ -102,7 +102,7 @@ const HeroSection = ({ lang }: { lang: keyof typeof Text }) => {
             delay: 0.5,
             duration: 0.75,
           }}
-          className="font-black block sm:text-5xl leading-[110%] tracking-tighter" data-value={Text[lang].title.last} ref={titleRef}
+          className="font-black block text-xl sm:text-5xl leading-[110%] tracking-tighter break-all" data-value={Text[lang].title.last} ref={titleRef}
         >
           {Text[lang].title.last}
         </motion.h1>
@@ -114,29 +114,34 @@ const HeroSection = ({ lang }: { lang: keyof typeof Text }) => {
             delay: 1,
             duration: 0.75,
           }}
-          className="max-w-lg mt-8 text-md sm:text-xl"
+          className="sm:max-w-lg mt-8 text-md sm:text-xl break-words"
         >
           {Text[lang].description}
         </motion.p>
 
-        <ul className="flex translate-y-20 gap-16">{stackIcons.map((icon, index) => (
+        <ul className="hidden sm:flex translate-y-20 gap-2 sm:gap-16">{stackIcons.map((icon, index) => (
           <motion.li
-
             initial={{ x: 100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{
               delay: 0.7 + (0.1 * index),
               duration: 0.75,
             }}
-
-            title={icon.title} key={icon.title} className="rounded-full p-1 flex items-center justify-center">{icon.icon}</motion.li>
-        ))}</ul>
+            title={icon.title} 
+            key={icon.title} 
+            className="rounded-full sm:p-1 flex items-center justify-center"
+            >
+              {icon.icon}
+            </motion.li>
+        ))}
+        </ul>
 
       </div>
-      <div className="absolute bottom-0 right-0 opacity-80 flex justify-end">
+
+      <div className="absolute bottom-0 right-0 opacity-80 flex justify-end overflow-hidden">
         <motion.div
-          initial={{ y: 25, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
+          initial={{ x: '100%', y: 40, opacity: 0 }}
+          animate={{ x: 30, y: 40, opacity: 1 }}
           transition={{
             delay: 1.25,
             duration: 0.75,
@@ -157,7 +162,6 @@ const HeroSection = ({ lang }: { lang: keyof typeof Text }) => {
             width="100%"
           />
         </motion.div>
-        <div className="absolute top-0 bottom-0 left-0 right-0" />
       </div>
     </section>
   );
