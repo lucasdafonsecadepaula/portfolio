45ad528424cc257c66886139591076a403a242b5
Lucas de Paula <lucasdafonsecadepaula@gmail.com>
2025-03-07 16:52:41 -0300

    fixing build erros.

--- src/app/[locale]/curriculum/page.tsx
+++ src/app/[locale]/curriculum/page.tsx
@@ -1,20 +1,20 @@
-import { useLocale } from 'next-intl'
+// import { useLocale } from 'next-intl'
 
 export default function Page() {
-  const locale = useLocale()
+  // const locale = useLocale()
 
-  const text =
-    locale === 'pt'
-      ? 'Bem-vindo ao meu currículo!'
-      : 'Welcome to my curriculum!'
-  const url =
-    locale === 'pt'
-      ? '/lucas_da_fonseca_de_paula.pdf'
-      : '/lucas_da_fonseca_de_paula-english_version.pdf'
+  // const text =
+  //   locale === 'pt'
+  //     ? 'Bem-vindo ao meu currículo!'
+  //     : 'Welcome to my curriculum!'
+  // const url =
+  //   locale === 'pt'
+  //     ? '/lucas_da_fonseca_de_paula.pdf'
+  //     : '/lucas_da_fonseca_de_paula-english_version.pdf'
 
   return (
     <div className="2xl:px-[8vw] py-16">
-      <h1 className="font-bold lg:text-[36px] text-[24px] leading-[42px] text-white mb-8">
+      {/* <h1 className="font-bold lg:text-[36px] text-[24px] leading-[42px] text-white mb-8">
         {text}
       </h1>
       <div className="h-screen">
@@ -23,7 +23,7 @@ export default function Page() {
           style={{ height: '100%', width: '100%', border: 'none' }}
           title="PDF Viewer"
         />
-      </div>
+      </div> */}
     </div>
   )
 }
